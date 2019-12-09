7fe0514e9c98970ccb86d67817751deb54a80756
Jason Crockett <JesusisLord33333@outlook.com>
2019-12-09 16:09:49 +0000

    Initial commit

---
+++ lib/ht/client/ruby.rb
@@ -0,0 +1,10 @@
+require "ht/client/ruby/version"
+
+module Ht
+  module Client
+    module Ruby
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "ht/client/ruby"
+
+require "minitest/autorun"
